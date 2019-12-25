a9e3249c2cde6caa57ec0c3b0258cfed6337f96e
khutjo maputla <39694088+khutjo@users.noreply.github.com>
2019-12-25 12:41:10 +0200

    Add files via upload

---
+++ src/makeanimation/Makeanimation.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package makeanimation;
+
+/**
+ *
+ * @author khutjo_laptop
+ */
+public class Makeanimation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+//        h.setsizedem();
+//        
+//        int height = h.getwinheight();
+//    
+//        int width = h.getwinwidth();
+//        
+//        System.out.println(h.getdems().getSize());
+//        while (true){
+////            if (height == h.getwinheight() && width == h.getwinwidth()){
+////                h.move(100, 100);
+////            }
+//            if (height != h.getwinheight()){
+//                height = h.getwinheight();
+//                h.move(100, height - 100);
+//            }
+//            if (width != h.getwinwidth()){
+//                System.out.println("what changer oh it's the width");
+//                width = h.getwinwidth();
+//            }
+//        }
+    }
+    
+}
